4cea96a0d718cc1ec02c4d1b631418297133a45d
Shairaaj <iamshairaaj@gmail.com>
2025-03-08 22:09:26 +0530

    vercel.json file added

--- frontend/src/components/PrivacyPolicy.jsx
+++ frontend/src/components/PrivacyPolicy.jsx
@@ -1,10 +1,11 @@
+// PrivacyPolicy.jsx
 import React, { useEffect, useState } from "react";
 
 const PrivacyPolicy = () => {
   const [content, setContent] = useState("Loading...");
 
   useEffect(() => {
-    fetch("https://swicall.onrender.com/privacy") // Fetch from backend
+    fetch("https://swicall.onrender.com/privacy")
       .then((response) => response.text())
       .then((data) => setContent(data))
       .catch((error) => setContent("Failed to load privacy policy."));
